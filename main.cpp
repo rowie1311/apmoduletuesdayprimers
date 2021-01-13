e9e87dc8f7359e5c3916f8a2e795df6d7102511c
Rowan Priest <rowan.priest@ada.ac.uk>
2021-01-13 22:28:52 +0000

    good start on tuesday primer 2

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int l_case=0, u_case=0, digit=0, special=0;
+  string str;
+  cout<<"Enter input string."<<endl;
+  
+  cin>>str;
+  int l=str.length(),i;
+  
+  for(i=0;i<l;i++)
+  {
+    if(islower(str[i]))
+      l_case=1;
+    if(isupper(str[i]))
+      u_case=1;
+    if(isdigit(str[i]))
+      digit=1;
+    if(!isalpha(str[i]) && !isdigit(str[i]))
+      special=1;  
+  }
+  
+  if(l_case && u_case && digit && special && l>=8)
+    cout<<"Strong password."<<endl;
+  else if((l_case+u_case+digit+special>=3) && l>=6)
+    cout<<"Moderate password"<<endl;
+  else
+    cout<<"Weak password"<<endl;
+    
+  
+  return 0;
+}
