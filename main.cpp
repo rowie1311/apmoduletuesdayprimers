83ffe5dc09d2306a9a993e945123a310c1e39b7f
Rowan Priest <rowan.priest@ada.ac.uk>
2021-01-18 16:13:24 +0000

    for loop broken otherwise complete - needs commenting

--- main.cpp
+++ main.cpp
@@ -6,29 +6,48 @@ using namespace std;
 int main() {
 	cout << "\nWelcome to Primer 4 - Account Validation\n\n";
 
+
 string usernameinput,passinput;
-string username[]={"mmm","nnnn","rrrr","aaa"};
-string password[]={"1234","1212","1234","1212"};
-bool authenticate=false;
+int attempts = 3;
+  while(1) {
+  cout << "\nPlease Enter Your Username And Password\n\n";
 
-while(authenticate==false){
-  cout << "Hello, Please Enter Your Username And Password\n";
-  cout << "username: "<<endl;
-    cin>>usernameinput;
-  cout << "password: "<<endl;
-    cin>>passinput;
-      for(int i=0;i<=3;i++){
-            if(usernameinput ==username[i] && passinput==password[i] )
-            {
-              authenticate=true;
-            }
-            else if (usernameinput !=username[i] && passinput!=password[i])
-            {
-              authenticate=false;
-              cout<<"please enter correct username and password"<<endl;
-            }
+  cout << "Username: "<<endl;
+  cin>>usernameinput;
+  cout << "Password: "<<endl;
+  cin>>passinput;
+  
 
-        };
+    string username[]={"mike","casper","rowan"};
+    string password[]={"1234","1212","8008"};
+  
+  
+
+    for (int i = 0; i >= 2; i++)
+    { //for loop will not work - hopefully can debug later 
+      if(usernameinput == username[i] && passinput == password[i] )
+      {
+        
+        cout << "Welcome! " << username[i];
+        return 0;
+        
+      }
+      
+      else if (usernameinput != username[i] && passinput != password[i] && attempts == 1)
+      {
+        cout << "Failed To Authenticate After 3 Attempts, Please Restart Program";
+        return 0;
+      }
+
+      else if (usernameinput != username[i] && passinput != password[i])
+      {
+        
+        cout<<"please enter correct username and password\n"<<endl;
+        attempts--;
+          cout << "You Have " << attempts << " Attempts Out Of 3 Remaining. ";
+          break;
+      
+      }
+    };
+  } 
 }
-}
-
